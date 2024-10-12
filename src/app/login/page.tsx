669bf27acca534478c196e8808e77f09d8b1777f
Alwyn 2 <alwynsiby@hotmail.com>
2024-10-12 11:27:36 +0100

    Login Page completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,12 @@
 "use client"
-import { useState } from 'react'
-import Modal from '@/components/modal'
 import { LoginWithLogo } from '@/components/login-components/login-with-logo'
 
 export default function LoginPage() {
-    const [isOpen, setIsOpen] = useState(false)
-
-    const openModal = () => setIsOpen(true)
-    const closeModal = () => setIsOpen(false)
-
     return (
-        <div className='min-h-screen px-4 sm:px-6 lg:px-8 relative'>
-            <button
-                onClick={openModal}
-                className='absolute top-4 left-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
-            >
-                Login
-            </button>
-
-            <Modal isOpen={isOpen} onClose={closeModal}>
-                <div className='w-full max-w-[350px] sm:max-w-md'>
-                    <LoginWithLogo />
-                </div>
-            </Modal>
+        <div className='min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8'>
+            <div className='w-full max-w-[350px] sm:max-w-md'>
+                <LoginWithLogo />
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import { Button } from "@/components/ui/button";
+"use client"
+import { useState } from 'react'
+import Modal from '@/components/modal'
+import { LoginWithLogo } from '@/components/login-components/login-with-logo'
 
-import { cookies } from "next/headers";
 export default function Home() {
-  return (
-    <Button>Login</Button>
-  );
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openModal = () => setIsOpen(true)
+    const closeModal = () => setIsOpen(false)
+
+    return (
+        <div className='min-h-screen px-4 sm:px-6 lg:px-8 relative'>
+            <button
+                onClick={openModal}
+                className='absolute top-4 left-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+            >
+                Login
+            </button>
+
+            <Modal isOpen={isOpen} onClose={closeModal}>
+                <div className='w-full max-w-[350px] sm:max-w-md'>
+                    <LoginWithLogo />
+                </div>
+            </Modal>
+        </div>
+    )
 }
