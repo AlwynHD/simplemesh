f590f5b44683cc5835d1ef11c92162eb1771e6bc
Alwyn 2 <alwynsiby@hotmail.com>
2024-10-23 12:38:28 +0100

    avatar fallback to first letter

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -33,11 +33,6 @@ interface User {
 }
 
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
   teams: [
     {
       name: "Simple Mesh",
