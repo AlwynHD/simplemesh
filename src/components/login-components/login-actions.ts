0b699e10727bb465bf395c262fadb9fdd678b5c4
Alwyn 2 <alwynsiby@hotmail.com>
2025-03-04 00:53:06 +0000

    added public site url

--- src/components/login-components/login-actions.ts
+++ src/components/login-components/login-actions.ts
@@ -54,7 +54,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `http://localhost:3000/auth/callback`,
+      redirectTo: `http://${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
