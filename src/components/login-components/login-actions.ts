d098c4944f7635c2f9bb0b34c03149861c4ce70e
Alwyn 2 <alwynsiby@hotmail.com>
2025-03-04 01:09:02 +0000

    changed back

--- src/components/login-components/login-actions.ts
+++ src/components/login-components/login-actions.ts
@@ -54,7 +54,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `http://simplemesh.ai/auth/callback`,
+      redirectTo: `http://${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
