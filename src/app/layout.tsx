e8d14059524b9cd775f8b0e654abdbeb4ca175a1
Alwyn <alwynsiby@hotmail.com>
2025-03-04 12:06:12 +0000

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,11 +32,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
       <body
-      
+
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
+
         {children}
       </body>
     </html>
