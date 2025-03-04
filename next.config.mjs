b82813793f14af0776660559b7dea4e71fdd7de5
Alwyn <alwynsiby@hotmail.com>
2025-03-04 11:43:17 +0000

    eslint

--- next.config.mjs
+++ next.config.mjs
@@ -16,13 +16,10 @@ const nextConfig = {
         },
       ],
     },
-  };
-  module.exports = {
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
       ignoreDuringBuilds: true,
-    },
-  }
+  },
+  };
+
   
   export default nextConfig;
