8383cdfd3c411cd94b40f72b65b29d725d98d0cc
Alwyn <alwynsiby@hotmail.com>
2025-03-25 12:38:52 +0000

    added indexes to site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,22 @@
 // next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.simplemesh.ai', // Replace with your domain
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-      policies: [
-        { userAgent: '*', allow: '/' },
-      ],
-    },
-    // Add other configuration options as needed
+  siteUrl: 'https://www.simplemesh.ai',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+  },
+  additionalPaths: async (config) => {
+    return [
+      { loc: '/dashboard', priority: 0.7, changefreq: 'daily' },
+      { loc: '/dashboard/image-3d', priority: 0.7, changefreq: 'daily' },
+      { loc: '/dashboard/text-3d', priority: 0.7, changefreq: 'daily' },
+      { loc: '/dashboard/guide', priority: 0.7, changefreq: 'daily' },
+      { loc: '/dashboard/models', priority: 0.7, changefreq: 'daily' },
+      { loc: '/dashboard/settings', priority: 0.7, changefreq: 'daily' },
+      // Add any other dashboard routes you want indexed
+    ]
   }
+}
